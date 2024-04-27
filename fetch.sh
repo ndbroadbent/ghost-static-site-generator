3eec00349b3d22d70984f5ed09c7e0f007a791eb
Nathan Broadbent <git@ndbroadbent.com>
2024-04-27 19:37:38 +1200

    update

--- fetch.sh
+++ fetch.sh
@@ -5,10 +5,10 @@ if ! [ -d "static" ]; then
   git clone git@github.com:ndbroadbent/ndbroadbent.github.io.git static
   git checkout gh-pages
 fi
-node src/index.js --domain https://ghost.ndbroadbent.com --productionDomain https://madebynathan.com
+node src/index.js --domain https://blog.ndbroadbent.com --productionDomain https://madebynathan.com
 
-# Replace all remaining instances of https://ghost.ndbroadbent.com with https://madebynathan.com in static files
-grep -rl "https://ghost.ndbroadbent.com" --include \*.html static | xargs sed -i '' 's/https:\/\/ghost.ndbroadbent.com/https:\/\/madebynathan.com/g'
+# Replace all remaining instances of https://blog.ndbroadbent.com with https://madebynathan.com in static files
+grep -rl "https://blog.ndbroadbent.com" --include \*.html static | xargs sed -i '' 's/https:\/\/blog.ndbroadbent.com/https:\/\/madebynathan.com/g'
 
 echo madebynathan.com > static/CNAME
 cp pubkey_38E63C0A.txt static/
