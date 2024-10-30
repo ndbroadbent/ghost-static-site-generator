d2fded81ada985a8c0d7466168dea22d406b7db1
Nathan Broadbent <git@ndbroadbent.com>
2024-10-30 14:53:54 +1300

    updates

--- update.sh
+++ update.sh
@@ -4,7 +4,11 @@ set -e
 rm -rf static/*
 ./fetch.sh
 
-cd static || exit
-git add .
-git commit -m "Update static files"
-git push origin gh-pages
+(
+  \cd static || exit
+  git add .
+  git commit -m "Update static files"
+  git push origin gh-pages
+)
+
+./purge_cloudflare_cache
